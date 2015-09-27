4092a09b8910baa759d61f7c31c5f9daac6d023f
Jose Suniaga <josemiguel@vauoo.com>
2015-09-27 05:05:42 +0000

    [REF] openacademy: adding calendar view to session

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,4 +1,5 @@
 from openerp import models, fields, api, exceptions
+from datetime import timedelta
 
 
 class Session(models.Model):
@@ -21,6 +22,9 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     active = fields.Boolean(default=True)
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
+
 
     @api.one
     @api.depends('seats','attendee_ids')
@@ -30,6 +34,29 @@ class Session(models.Model):
         else:
             self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
 
+    @api.one
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+
+        # Compute the difference between dates, but: Friday - Monday = 4
+        # days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
