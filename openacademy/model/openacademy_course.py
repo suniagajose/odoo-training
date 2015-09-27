b3de8ec6c4ea4e1535c582645094c1436d23691e
Jose Suniaga <josemiguel@vauoo.com>
2015-09-27 02:20:18 +0000

    [REF] openacademy: Inherit copy function in course model

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -1,4 +1,4 @@
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class Course(models.Model):
@@ -26,3 +26,14 @@ class Course(models.Model):
          "The course title must be unique"),
     ]
 
+    @api.one # api.one send defaults params: cr, uid, id, context
+    def copy(self, default=None):
+        default = dict(default or {})
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+        default['name'] = new_name
+        return super(Course, self).copy(default)
