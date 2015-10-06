afb11b309ea7b58afbb3b2f7147f2814d0cab290
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-06 22:19:21 +0000

    [IMP] openacademy: adding wkf test to session

--- openacademy/tests/test_global_openacademy_session.py
+++ openacademy/tests/test_global_openacademy_session.py
@@ -12,6 +12,7 @@ class GlobalTestOpenAcademySession(TransactionCase):
         self.partner_vauxoo = self.env.ref('base.res_partner_23')
         self.session = self.env['openacademy.session']
         self.course = self.env.ref('openacademy.course1')
+        self.partner_attende = self.env.ref('base.res_partner_5')
 
     # Generic methods
 
@@ -32,3 +33,26 @@ class GlobalTestOpenAcademySession(TransactionCase):
                 'attendee_ids': [(6, 0, [self.partner_vauxoo.id])],
                 'course_id': self.course.id
             })
+
+    def test_20_wkf_done(self):
+        '''
+        Check that the workflow work fine!
+        '''
+        session_test = self.session.create({
+            'name': 'Session test 1',
+            'seats': 2,
+            'instructor_id': self.partner_vauxoo.id,
+            'attendee_ids': [(6, 0, [self.partner_attende.id])],
+            'course_id': self.course.id,
+        })
+
+        # Check initial state
+        self.assertEqual(session_test.state, 'draft', 'Initial state should be in "draft"')
+
+        # Change next state and check it
+        session_test.signal_workflow('button_confirm')
+        self.assertEqual(session_test.state, 'confirmed', "Signal confirm don't work fine!")
+
+        # Change next state and check it
+        session_test.signal_workflow('button_done')
+        self.assertEqual(session_test.state, 'done', "Signal done don't work fine!")
