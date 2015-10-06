35ba35442934717487a10d4110bb641c5dc5a3d7
Jose Suniaga <josemiguel@vauxoo.com>
2015-10-06 21:42:41 +0000

    [IMP] openacademy: adding same name test to course

--- openacademy/tests/test_global_openacademy_course.py
+++ openacademy/tests/test_global_openacademy_course.py
@@ -45,3 +45,17 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
             # Create a course with same name and description to raise error.
             self.create_course('test', 'test', None)
 
+    @mute_logger('openerp.sql_db')
+    def test_20_two_courses_same_name(self):
+        '''
+        Test to create two courses with same name.
+        To raise constraint of unique name
+        '''
+        new_id = self.create_course('test1', 'test_description', None)
+        # print "new_id", new_id
+        with self.assertRaisesRegexp(
+                IntegrityError,
+                'duplicate key value violates unique'
+                ' constraint "openacademy_course_name_unique"'
+                ):
+            new_id2 = self.create_course('test1', 'test_description', None)
